528bb60abd631422f7b429bf31f3ee9cc2501d8f
Mikhail Makartsev <m.p.makartsev@gmail.com>
2024-09-12 23:00:23 +0600

    Fix a colored noise bug on black and high saturation pixels

--- falsecolor.cpp
+++ falsecolor.cpp
@@ -160,20 +160,54 @@ public:
 #if 1
     float dist(const Image& other) const
     {
-        __m256 dr = _mm256_setzero_ps();
-        __m256 dg = _mm256_setzero_ps();
-        __m256 db = _mm256_setzero_ps();
+        __m256 out = _mm256_setzero_ps();
 
-        __m256* data = (__m256*)m_data;
-        __m256* other_data = (__m256*)other.m_data;
+        const __m256 const2 = _mm256_set1_ps(2.0f);
+        const __m256 const4 = _mm256_set1_ps(4.0f);
+        const __m256 const0_5 = _mm256_set1_ps(0.5f);
+        const __m256 const255 = _mm256_set1_ps(255.0f);
+        const __m256 const256 = _mm256_set1_ps(256.0f);
 
-        for (size_t i = 0; i < m_data_size / 8; i += 3) {
-            dr = _mm256_add_ps(dr, calc_dist(data[i], other_data[i]));
-            dg = _mm256_add_ps(dg, calc_dist(data[i + 1], other_data[i + 1]));
-            db = _mm256_add_ps(db, calc_dist(data[i + 2], other_data[i + 2]));
+        __m256 avg_r, dr, dg, db, dr_sqr, dg_sqr, db_sqr, coef_r, coef_g, coef_b, dist_r, dist_g, dist_b;
+        std::array<float, 8> r1_arr, g1_arr, b1_arr, r2_arr, g2_arr, b2_arr;
+        // Assumption! Image width * height % 8 == 0
+        for (size_t i = 0; i < m_data_size; i += 24) {
+            for (size_t j = 0; j < 8; j++) {
+                r1_arr[j] = m_data[i + 3 * j];
+                g1_arr[j] = m_data[i + 3 * j + 1];
+                b1_arr[j] = m_data[i + 3 * j + 2];
+                r2_arr[j] = other.m_data[i + 3 * j];
+                g2_arr[j] = other.m_data[i + 3 * j + 1];
+                b2_arr[j] = other.m_data[i + 3 * j + 2];
+            }
+            __m256 r1 = *((__m256*) r1_arr.data());
+            __m256 g1 = *((__m256*) g1_arr.data());
+            __m256 b1 = *((__m256*) b1_arr.data());
+            __m256 r2 = *((__m256*) r2_arr.data());
+            __m256 g2 = *((__m256*) g2_arr.data());
+            __m256 b2 = *((__m256*) b2_arr.data());
+
+            avg_r = _mm256_mul_ps(const0_5, _mm256_add_ps(r1, r2));
+            dr = _mm256_sub_ps(r1, r2);
+            dg = _mm256_sub_ps(g1, g2);
+            db = _mm256_sub_ps(b1, b2);
+            dr_sqr = _mm256_mul_ps(dr, dr);
+            dg_sqr = _mm256_mul_ps(dg, dg);
+            db_sqr = _mm256_mul_ps(db, db);
+            coef_r = _mm256_add_ps(const2, _mm256_div_ps(avg_r, const256));
+            coef_g = const4;
+            coef_b = _mm256_add_ps(const2, _mm256_div_ps(_mm256_sub_ps(const255, avg_r), const256));
+
+            dist_r = _mm256_mul_ps(coef_r, dr_sqr);
+            dist_g = _mm256_mul_ps(coef_g, dg_sqr);
+            dist_b = _mm256_mul_ps(coef_b, db_sqr);
+            out = _mm256_add_ps(out, _mm256_mul_ps(dist_r, dist_r));
+            out = _mm256_add_ps(out, _mm256_mul_ps(dist_g, dist_g));
+            out = _mm256_add_ps(out, _mm256_mul_ps(dist_b, dist_b));
+            //out = _mm256_add_ps(out, dist_r);
+            //out = _mm256_add_ps(out, dist_g);
+            //out = _mm256_add_ps(out, dist_b);
         }
-
-        __m256 out = _mm256_add_ps(dr, _mm256_add_ps(dg, db));
 
         float ds = 0.0f;
         for (int i = 0; i < 8; i++) {
@@ -187,12 +221,25 @@ public:
     {
         float d = 0.0f;
 
-        for (size_t i = 0; i < m_data_size; i++) {
-            float a = m_data[i];
-            float b = other.m_data[i];
-            float delta = fabs(a - b);
+        for (size_t i = 0; i < m_data_size; i += 3) {
+            float r1 = m_data[i];
+            float g1 = m_data[i + 1];
+            float b1 = m_data[i + 2];
+            float r2 = other.m_data[i];
+            float g2 = other.m_data[i + 1];
+            float b2 = other.m_data[i + 2];
+            float avg_r = (r1 + r2) / 2.0f;
+            float dr = fabs(r1 - r2);
+            float dg = fabs(g1 - g2);
+            float db = fabs(b1 - b2);
+            float coef_r = 2.0f + avg_r;
+            float coef_g = 4.0f;
+            float coef_b = 3.0f - avg_r;
+            float delta_r = coef_r * dr;
+            float delta_g = coef_g * dg;
+            float delta_b = coef_b * db;
 
-            d += a * b * delta * delta * delta * delta * delta;
+            d += r1 * r2 * pow(delta_r, 5) + g1 * g2 * pow(delta_g, 5) + b1 * b2 * pow(delta_b, 5);
         }
 
         return d;
